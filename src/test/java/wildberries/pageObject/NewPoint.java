9818906ba77203001b37261d98cfaa10b84eb2bc
stassi97 <@novikstassi@gmail.com>
2024-03-15 19:00:47 +0300

    New Locator for PAGE_LOCATOR

--- src/test/java/wildberries/pageObject/NewPoint.java
+++ src/test/java/wildberries/pageObject/NewPoint.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 public class NewPoint extends BasePage {
 
-    private static final String PAGE_LOCATOR = "//body[@class='theme-pink event--wb19 is-on-homepage is-hidden']";
+    private static final String PAGE_LOCATOR = "//div[@data-tag='pvzListRoot']";
 
     private static final Label newPoint= new Label(By.xpath("//div[@class='pvz-item-delivery__address' and text()='Минск Улица Павлины Медёлки 3']"));
 

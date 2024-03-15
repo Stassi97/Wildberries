3391f29d775a2224ffc1474396981b0eea1c08a9
stassi97 <@novikstassi@gmail.com>
2024-03-15 20:09:17 +0300

    New Locator for ComparePage.java

--- src/test/java/wildberries/pageObject/ComparePage.java
+++ src/test/java/wildberries/pageObject/ComparePage.java
@@ -9,16 +9,18 @@ import org.testng.asserts.SoftAssert;
 public class ComparePage extends BasePage {
     SoftAssert softAssert = new SoftAssert();
     private static final String PAGE_LOCATOR = "//div[@class='catalog__in container']";
-    private static final String ITEM_NAME = "//span[@class='product-card__name']";
+    private static final String ITEM_NAME = "//div[@class='product-card__title']";
     private static final String FILTER_ITEM_NAME = "//span[@class='chip__name' and text()='Наушники']";
     public ComparePage() {
-        super(By.xpath(PAGE_LOCATOR),"'Check Product filtration' Page");
+        super(By.xpath(PAGE_LOCATOR),"'Check Product name' Page");
     }
 
-    @Step("Check product")
+    @Step("Check product name")
     public void compareProductItem() {
         String itemName = getDriver().findElement(By.xpath(ITEM_NAME)).getText();
         String filterName = getDriver().findElement(By.xpath(FILTER_ITEM_NAME)).getText();
+        System.out.println(itemName);
+        System.out.println(filterName);
         softAssert.assertTrue(itemName.contains(filterName), "Wrong name");
         softAssert.assertAll();
     }
